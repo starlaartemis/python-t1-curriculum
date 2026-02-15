ea914193406df8c8f81729ebd62daf26e2e91341
starlaartemis <starlaartemis@gmail.com>
2026-02-15 02:04:04 +0000

    lesson number 1

---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,8 @@
+x = 5 # integer: a whole number wiyhout a desimal 
+print (x)
+
+favorite_food = "mac n cheese" # string: text enclosed in quotes 
+print(favorite_food)
+
+y = 3.25 #float: number with a decimal point 
+print(y,"dogs",4)
