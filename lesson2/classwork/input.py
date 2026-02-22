0ac714fd5b3f143301a3d44b1ea5228d8cd1f9d3
starlaartemis <starlaartemis@gmail.com>
2026-02-21 18:10:58 -0800

    Update input.py

---
+++ lesson2/classwork/input.py
@@ -0,0 +1,9 @@
+user_input = input("give me and integer:")
+print(user_input)
+
+print(user_input + 1)
+
+number = int(user_input)
+
+user_number = int(input("give me an integer:"))
+print(user_number + 1)
