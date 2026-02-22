dccf47d689e59ae90cae40b8a89bd9d302d6bd1e
starlaartemis <starlaartemis@gmail.com>
2026-02-21 17:35:40 -0800

    Update casting.py

---
+++ lesson2/classwork/casting.py
@@ -0,0 +1,15 @@
+num_str = "42"
+num_int = int(num_str) # Converting a string to an integer
+print(num_int + 1)
+
+num_str = "3.14"
+num_float = float(num_str)
+print(num_float)
+
+float_num = 9.99
+int_numb = int(float_num)
+print(int_num)
+
+num = 100
+num_str2 = str(num)
+print(num_str)
